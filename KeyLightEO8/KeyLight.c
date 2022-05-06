d263afd3b5cbde03c617e6cf87d08ba3ec194488
thtfcccj <thtfcccj@qq.com>
2022-05-06 21:59:10 +0800

    KeyLight：   1. 将其与EO8模块解耦合，以使其适用面更宽。   2. 增加测试功能支持 上述修改测试完成。

--- KeyLightEO8/KeyLight.c
+++ KeyLightEO8/KeyLight.c
@@ -1,12 +1,11 @@
 /*****************************************************************************
 
-		              扫描时置指示灯与获得按键-在EO8(如595)上的实现
+		              扫描时置指示灯与获得按键-在在8位输出上的实现
 
 ******************************************************************************/
 
 #include "KeyLight.h"
 #include <string.h>
-#include "EO8.h"
 
 struct _KeyLight KeyLight; //直接单例化
 
@@ -21,8 +20,7 @@ static const unsigned char _ScanLut[] = KeyLight_cbScanLut();
 void KeyLight_Init(void)
 {
   memset(&KeyLight, 0, sizeof(struct _KeyLight));
-  KeyLight_cbInitIo();  
-  EO8_Init();
+  KeyLight_cbInitIo();
 }
 
 //-------------------------中断任务函数------------------------------------
@@ -37,10 +35,12 @@ void KeyLight_IntTask(void)
   if(ScanPos >= KEY_LIGHT_SCAN_COUNT) ScanPos = 0;
   KeyLight.ScanPos = ScanPos;
   //获得需更新的数据
-  unsigned char Data = KeyLight.Light[ScanPos] & KeyLight_cbLightMask(); //指示灯列
+  unsigned char Data;
+  if(KeyLight_cbIsTest()) Data = KeyLight_cbLightOn(); //指示灯全亮
+  else Data = KeyLight.Light[ScanPos] & KeyLight_cbLightMask(); //指示灯列
   Data |= _ScanLut[ScanPos] | KeyLight_cbGetFixBit(); //扫描位及固定位更新
   //一起更新了
-  EO8_SetAllFast(Data);
+  KeyLight_cbUpdateOut(Data);
 }
 
 
--- KeyLightEO8/KeyLight.h
+++ KeyLightEO8/KeyLight.h
@@ -1,9 +1,9 @@
 /*****************************************************************************
 
-		              扫描时置指示灯与获得按键-在EO8(如595)上的实现
+		              扫描时置指示灯与获得按键-在8位输出上的实现
 //此模块驱动的硬件结构为：
 //最大4X4阵列的指示灯与按键
-//按键与指示灯行选扫描复用EO8(如595)输出，指示灯列显用EO8(如595)输出
+//按键与指示灯行选扫描复用8位输出(如EO8驱动595)输出
 //读按键列用IO实现
 ******************************************************************************/
 
@@ -65,16 +65,26 @@ void KeyLight_IntTask(void);
 #include "IoCtrl.h"  //部分直接IO实现:
 //IO初始化                        
 //#define KeyLight_cbInitIo()   do{}while(0)                       
-//定义EO8(如595)上，指示灯与按键的扫描掩码    
+//定义(如EO8驱动595)上，指示灯与按键的扫描掩码    
 //#define KeyLight_cbScanLut()   {0xnn, 0xnn,0xnn} 
-//EO8上有其它固定位时，该位的值，无时为0
+//8位输出上有其它固定位时，该位的值，无时为0
 //#define KeyLight_cbGetFixBit()    0
-//定义EO8(如595)上，指示灯位置掩码    
+//定义8位输出(如EO8驱动595)上，指示灯位置掩码    
 //#define KeyLight_cbLightMask()   0xnn
+//定义为指示灯亮  
+//#define KeyLight_cbLightOn()   0xnn
+
+//更新输出
+//#define KeyLight_cbUpdateOut(out)   do{}while(0)     
 
 //----------------------------得到当前键值----------------------------------
 //对应位为1表示按下了
 unsigned char KeyLight_cbGetKeyVol(void);
+
+//----------------------------是否测试状态----------------------------------
+#include "Test.h"  //直接实现
+#define KeyLight_cbIsTest()  Test_IsDoing()
+
 
 #endif //#ifdef _KEY_LIGHT_H
 
